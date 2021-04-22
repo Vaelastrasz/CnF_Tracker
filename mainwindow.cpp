ea78709eb954d6f4b08c2c66db1db1a0436d78a9
Vlad Chobanitsa <chobanitsavlad@gmail.com>
2021-04-22 12:42:20 +0300

    Added stylesheet; Added minor startup

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,14 +5,40 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+    ui(new Ui::MainWindow) {
+
     ui->setupUi(this);
-    this->setWindowTitle(TEXT("C&F Tracker"));
-    this->setWindowIcon(QIcon(":/icons/truck.png"));
+    startup();
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::on_actionMaximizeWindow_toggled(bool arg1) {
+    if (arg1) {
+        this->setWindowState(Qt::WindowMaximized);
+    } else {
+        this->setWindowState(Qt::WindowNoState);
+    }
+}
+
+void MainWindow::on_actionMinimizeWindow_triggered() {
+    this->setWindowState(Qt::WindowMinimized);
+}
+
+void MainWindow::startup() {
+
+    this->setWindowTitle(TEXT("C&F Tracker"));
+    this->setWindowIcon(QIcon(":/icons/truck.png"));
+    ui->btn_addRecord->setIcon(QIcon(":/icons/plus.png"));
+    applyStyleSheet();
+}
+
+void MainWindow::applyStyleSheet() {
+    QFile style(":/Medize.qss");
+    style.open(QIODevice::ReadOnly);
+    QString styleSheet = QString(style.readAll());
+    style.close();
+    this->setStyleSheet(styleSheet);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
 
 namespace Ui {
 class MainWindow;
@@ -15,7 +16,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_actionMaximizeWindow_toggled(bool arg1);
+
+    void on_actionMinimizeWindow_triggered();
+
 private:
+    void startup();
+    void applyStyleSheet();
+
     Ui::MainWindow *ui;
 };
 
