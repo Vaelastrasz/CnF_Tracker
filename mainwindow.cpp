ef1ab6e08da76e6d782d012c4ddf875c9e0fe731
Vlad Chobanitsa <chobanitsavlad@gmail.com>
2021-04-22 12:09:29 +0300

    Completed UI prototype; Added truck logo; Added shortcut logos; Added title

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#define TEXT(x) tr(x)
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle(TEXT("C&F Tracker"));
+    this->setWindowIcon(QIcon(":/icons/truck.png"));
 }
 
 MainWindow::~MainWindow()
